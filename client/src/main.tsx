d937b49227bf375ecdf45ffc84800ec99259549f
jumalaw98 <jumalawrence98@gmail.com>
2025-12-01 23:44:31 +0300

    Update on csp headers  Changes to be committed: 	modified:   client/index.html 	modified:   client/src/main.tsx 	modified:   vercel.json

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ✅ Import the advanced security middleware
 
 import { enableAdvancedSecurity } from "@/security/securityMiddleware.ts"
-// 🔐 Run all frontend security features in production
 if (import.meta.env.PROD) {
     enableAdvancedSecurity();
 }
